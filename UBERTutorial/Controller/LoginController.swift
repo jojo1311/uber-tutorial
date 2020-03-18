9b3c519b493297cf3e4d164782211aa8ac936f9b
jojo1311 <josette@cocuyostudio.com>
2020-03-18 19:02:03 -0400

    modified LoginController and added new one

--- UBERTutorial/Controller/LoginController.swift
+++ UBERTutorial/Controller/LoginController.swift
@@ -20,41 +20,104 @@ class LoginController: UIViewController {
         label.textColor = UIColor(white: 1, alpha: 0.20)
         return label
     }()
-    
-    private let emailContainerView: UIView = {
-        let view = UIView()
+    // emailContainerView was declared outside of the view emailContainerView, now
+    // we need to change the Let for a lazy var
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
         return view
     }()
     
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return view
+       }()
+    
     private let emailTextField: UITextField = {
-        let tf = UITextField()
-        
-        tf.borderStyle = .none
-        tf.font = UIFont.systemFont(ofSize: 16)
-        tf.textColor = .white
-        tf.keyboardAppearance = .dark
-        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor:UIColor.lightGray])
-        return tf
+        return UITextField().textField(withPlaceholder: "Email",
+                                       isSecureTextEntry: false)
     }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password",
+                                       isSecureTextEntry: true)
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(UIColor(white: 1, alpha: 0.5), for: .normal)
+        button.backgroundColor = .mainBlueTint
+        button.layer.cornerRadius = 5
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        return button
+    }()
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ",
+                              attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        attributedTitle.append(NSAttributedString(string: "Sign Up",
+                              attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.mainBlueTint]))
+        
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+ 
     
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
-        //add label to view
-        view.addSubview(titleLabel)
-        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
-        titleLabel.centerX(inView: view)
+        configureUI()
+      
     }
     
     //changing status bar
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent
     }
-
-
+    
+    // MARK: - Selectors
+    
+    @objc func handleShowSignUp() {
+        print("Attempting to push controller...")
+        let controller = SignUpController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    //MARK: - Helper Functions
+    
+    func configureUI() {
+        
+        view.backgroundColor = .backgroundColor
+        //add label to view
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+         
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                    passwordContainerView,
+                                                    loginButton])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 24
+         
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16)
+         
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, height: 32)
+    }
+    func configureNavigationBar(){
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
 }
 
---
+++ UBERTutorial/Controller/SignUpController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpController.swift
+//  UBERTutorial
+//
+//  Created by josette rojas on 3/18/20.
+//  Copyright © 2020 Josette Rojas. All rights reserved.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+}
